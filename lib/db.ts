0cc8771df3a13436337e5accc2477ff7c012048b
Sasha Goloshchapov <ferrata@hey.com>
2026-04-28 00:16:18 -0500

    feat: switched to sqlite

--- lib/db.ts
+++ lib/db.ts
@@ -1,30 +1,3 @@
-// import { Db } from '../types';
-// import * as firebaseDB from './dbs/firebase';
-import * as memoryDB from './dbs/memory';
-// import * as sqlDB from './dbs/mysql';
+import * as sqliteDB from './dbs/sqlite';
 
-export default memoryDB;
-
-// const { DB_TYPE } = process.env;
-
-// console.log(`Using ${DB_TYPE} database`);
-
-// let db: Db;
-
-// switch (DB_TYPE) {
-//     case 'firebase':
-//         db = firebaseDB;
-//         break;
-//     case 'in-memory':
-//     case 'memory':
-//         db = memoryDB;
-//         break;
-//     case 'mysql':
-//         db = sqlDB;
-//         break;
-//     default:
-//         db = firebaseDB;
-//         break;
-// }
-
-// export default db;
+export default sqliteDB;
