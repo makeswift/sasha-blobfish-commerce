bd8bebb03c23273dfd19a2c57bd140763eb2cfe9
Sasha Goloshchapov <ferrata@hey.com>
2026-04-28 10:16:28 -0500

    feat: added alert toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,24 @@ import { theme as defaultTheme } from '@bigcommerce/big-design-theme'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import Header from '../components/header'
+import { AlertsProvider } from '../context/alerts'
 import SessionProvider from '../context/session'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Box
-        marginHorizontal={{ mobile: 'none', tablet: 'xxxLarge' }}
-        marginVertical={{ mobile: 'none', tablet: 'xxLarge' }}
-      >
-        <Header />
-        <SessionProvider>
-          <Component {...pageProps} />
-        </SessionProvider>
-      </Box>
+      <AlertsProvider>
+        <Box
+          marginHorizontal={{ mobile: 'none', tablet: 'xxxLarge' }}
+          marginVertical={{ mobile: 'none', tablet: 'xxLarge' }}
+        >
+          <Header />
+          <SessionProvider>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </Box>
+      </AlertsProvider>
     </ThemeProvider>
   )
 }
